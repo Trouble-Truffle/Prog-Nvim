5fd17e4fadab6291d5fa618cf19714590de0b58c
Trouble-Truffle <perigordtruffle7318@gmail.com>
2022-06-17 08:18:12 +0800

    Added a dummy setup function

--- lua/prog/init.lua
+++ lua/prog/init.lua
@@ -1,5 +1,28 @@
 local M = {}
 
-print("init.lua\n\n\n")
+
+local hasCoderunner, _ = pcall(require, "code_runner")
+
+local defaults = {
+  projectFileLocation = vim.fn.stdpath('data') .. '/projects/projects',
+  autoGenerate = {
+    enable = true,
+    codeRunner = hasCoderunner
+  }
+}
+
+local opts = {}
+M.setup = function(userOpts)
+  opts = vim.tbl_deep_extend("force", defaults, userOpts or {})
+end
+
+M.create = function(path)
+  require("prog.reify").create(opts, path or opts.projectFileLocation)
+end
+
+M.test = function()
+  M.create()
+end
+
 
 return M
--- lua/prog/setup.lua
+++ lua/prog/setup.lua
@@ -1,14 +1,3 @@
 local M = {}
 
-M.defaults = {
-  exclude = {
-     lsp = {},
-     ft = {}
-    }
-}
-
-M.setup = function(opts)
-  M.opts = vim.tbl_deep_extend("force", M.defaults, opts)
-end
-
 return M
